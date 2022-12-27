bc84efd3ac3e2cd0fdae4da7403ffc08a53c2ac8
Vinicius Gabriel Almeida <83517746+VGabriel-7@users.noreply.github.com>
2022-12-27 19:26:06 -0300

    Corrigindo lint do req 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,6 +1,6 @@
 db.produtos.find({
   valoresNutricionais: { $elemMatch: {
     tipo: "proteínas",
-    $and: [{quantidade: { $gte: 30 }}, {quantidade: { $lte: 40 }}],
+    $and: [{ quantidade: { $gte: 30 } }, { quantidade: { $lte: 40 } }],
   } } },
   { nome: 1, _id: 0 });
