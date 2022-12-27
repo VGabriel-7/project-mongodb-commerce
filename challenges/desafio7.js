c52aaa2cb66899a56bd2806dfa904c7b57f15d91
Vinícius Gabriel <viniciusgsa99@gmail.com>
2022-12-27 18:17:36 -0300

    Requisito 8 concluído. Deletando produtos que tem menos de 50 curtidas e mostrando os produtos que sobraram no banco.

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,4 +1,3 @@
-// o nome e vendidos dos lanches que tenham sido vendidos com uma quantidade diferente de 50 e em que o campo tags não exista
 db.produtos.find(
   {
     $and: [{ vendidos: { $ne: 50 } }, { tags: { $exists: false } }],
