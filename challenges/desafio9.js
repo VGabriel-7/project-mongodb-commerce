75737f203aacbce2e37f582c115652cb8227c72c
Vinícius Gabriel <viniciusgsa99@gmail.com>
2022-12-27 18:50:47 -0300

    Corrigindo requisito 9.

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,1 +1,7 @@
-db.produtos.find({ calorias: { $lt: 500 } }, { nome: 1, _id: 0 });
+db.produtos.find({
+  valoresNutricionais: { $elemMatch: {
+    tipo: "calorias",
+    quantidade: { $lt: 500 },
+  } } },
+  { nome: 1, _id: 0 },
+);
