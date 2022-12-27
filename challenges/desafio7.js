9749708c5fc9f5364514c06f20533d1476259678
Vinícius Gabriel <viniciusgsa99@gmail.com>
2022-12-27 16:39:14 -0300

    Corrigindo Lint.

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,4 +1,4 @@
 // o nome e vendidos dos lanches que tenham sido vendidos com uma quantidade diferente de 50 e em que o campo tags não exista
 db.produtos.find({
-    $and: [{ vendidos: { ne: 50 } }, {tags: { $exists: false }}],
+    $and: [{ vendidos: { ne: 50 } }, { tags: { $exists: false }}],
   }, { nome: 1, vendidos: 1, _id: 0 });
