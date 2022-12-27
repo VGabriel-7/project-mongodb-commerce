68c49e28f5234dc867725023116b256992dea829
Vinícius Gabriel <viniciusgsa99@gmail.com>
2022-12-27 19:31:54 -0300

    Requisito 10 correção, eu acho.

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,6 +1,6 @@
 db.produtos.find({
   valoresNutricionais: { $elemMatch: {
     tipo: "proteínas",
-    $and: [{quantidade: { $gte: 30 }}, {quantidade: { $lte: 40 }}],
+    percentual: { $gte: 30, $lte: 40 },
   } } },
   { nome: 1, _id: 0 });
