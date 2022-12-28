42ebdfaa208c14783beb23b1acfbc6304bcd0b9c
Vinícius Gabriel <viniciusgsa99@gmail.com>
2022-12-28 11:22:06 -0300

    Requisito 15 concluído e requisito 13 corrigido. Querys para adicionar o campo avaliacao em todos os documentos da coleção e efetue alterações nesse campo.

---
+++ challenges/desafio15.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany({}, { avaliacao: 0 });
+
+db.produtos.updateMany(
+  { tags: { $elemMatch: { $eq: "bovino" } } },
+  { $inc: { avaliacao: 5 } },
+);
+
+db.produtos.updateMany(
+  { tags: { $elemMatch: { $eq: "ave" } } },
+  { $inc: { avaliacao: 3 } },
+);
+
+db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 });
+
