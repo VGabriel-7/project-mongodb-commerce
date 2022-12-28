006a449f5ae3d23191cb100a5757f037c012950c
Vinícius Gabriel <viniciusgsa99@gmail.com>
2022-12-28 09:52:19 -0300

    Requisito 11 concluído. Query para retornar o nome do produto, a quantidade de curtidas e quantos itens foram vendidos dos produtos que não sejam iguais a Big Mac e McChicken.

---
+++ challenges/desafio11.js
@@ -0,0 +1,5 @@
+// Retorne o nome do produto, a quantidade de curtidas e quantos itens foram vendidos dos produtos que não sejam iguais a Big Mac e McChicken
+db.produtos.find(
+  { nome: { $nin: ["Big Mac", "McChicken"] } },
+  { nome: 1, curtidas: 1, vendidos: 1, _id: 0 },
+);
