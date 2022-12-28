75b90f001feb2e7bf1c24961ca34c0a83a24f544
Vinícius Gabriel <viniciusgsa99@gmail.com>
2022-12-28 11:41:06 -0300

    Corrigindo requisito 15, eu acho.

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,14 +1,13 @@
 db.produtos.updateMany({}, { $set: { avaliacao: 0 } });
 
 db.produtos.updateMany(
-  { tags: { $elemMatch: { $eq: "bovino" } } },
-  { $inc: { avaliacao: 5 } },
+  { tags: { $in: ["bovino"] } },
+  { $set: { avaliacao: 5 } },
 );
 
 db.produtos.updateMany(
-  { tags: { $elemMatch: { $eq: "ave" } } },
-  { $inc: { avaliacao: 3 } },
+  { tags: { $in: ["ave"] } },
+  { $set: { avaliacao: 3 } },
 );
 
 db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 });
-
