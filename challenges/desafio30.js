95660ae71656f0834f0950eb14eb5aa46fb54b4f
Vinícius Gabriel <viniciusgsa99@gmail.com>
2022-12-29 10:49:51 -0300

    Corrigindo requisito 30, eu acho.

--- challenges/desafio30.js
+++ challenges/desafio30.js
@@ -1,9 +1,9 @@
 db.produtos.updateOne(
-  {},
+  { nome: "Big Mac" },
   { $unset: { curtidas: "" } },
 );
 
 db.produtos.find(
-  { nome: { $nin: ["Big Mac"] } },
+  {},
   { nome: 1, curtidas: 1, _id: 0 },
 );
